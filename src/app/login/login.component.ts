71814f300f4d1a76b381f78eccb9a462ccc9846d
sai kiran <sainik18@gmail.com>
2020-12-03 22:59:24 +0530

    Login func added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { AppService } from '../app.service';
 
 @Component({
@@ -10,7 +11,8 @@ import { AppService } from '../app.service';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder, private service: AppService,
+  ErrText: any;
+  constructor(private formBuilder: FormBuilder, private service: AppService, private router: Router,
     public dialog: MatDialog, public dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit(): void {
@@ -22,7 +24,14 @@ export class LoginComponent implements OnInit {
 
   onSubmit(){
     this.service.login(this.loginForm.value).subscribe((resp) => {
-      console.log(resp);
+      this.ErrText = '';
+      if(resp.status){
+        localStorage.setItem('token', resp.token);
+        this.router.navigate(['/dashboard']);
+        this.dialogRef.close();
+      }else {
+        this.ErrText = resp.msg;
+      }
     })
   }
 
